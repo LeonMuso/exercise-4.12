060eeae318c2d4a03237e5bc6344b77daebd3d48
leomuso <Pinosenx@gmail.com>
2024-10-15 18:14:02 +0200

    exercise 4.12

--- main.c
+++ main.c
@@ -8,10 +8,13 @@ int main(void) {
     scanf("%d", &b);
     printf(" inserisci i valori di num:");
     scanf("%d", &num);
-    int i = a;
-    while (i >= a && i <= b) {
+    int i /*= a*/;
+    /*while (i >= a && i <= b) {
         printf("i multipli di num sono: %d\n" ,num * i);
         i++;
+    }*/
+    for (i = a; i <= b; i++) {
+        printf("i multipli di num sono: %d\n" ,num * i);
     }
     return 0;
 }
